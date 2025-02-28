e856f7d4ac5d45e62c3454f596695fe0ed8ac94a
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2025-02-28 22:12:18 +1100

    feat(backend): initialize Express server

---
+++ inato-menu-backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
